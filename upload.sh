2a7c600c6c05d32c3c664268534dbf111aec28ee
Gradyn Nagle <55261229+gradynn@users.noreply.github.com>
2023-02-21 21:56:01 -0800

    removed clutter

--- upload.sh
+++
@@ -1,9 +0,0 @@
-test_name='test_preempt' 
-
-echo "Enter CSIF username: "
-read username
-ssh $username@pc02.cs.ucdavis.edu "rm -rf ecs150-p2"
-echo "Remnants of previous upload removed"
-scp -rq ../ecs150-p2 $username@pc02.cs.ucdavis.edu:~/ecs150-p2
-echo "New upload complete"
-ssh $username@pc02.cs.ucdavis.edu "cd ./ecs150-p2/apps ; make clean ; cd ../libuthread ; make ; cd ../apps ; make $test_name.x ; ./$test_name.x" 
